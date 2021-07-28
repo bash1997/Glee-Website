b04cc95464fc317061cf2b89b17bc9d22b1de6fa
Bradley Lau <79525926+BradLau206@users.noreply.github.com>
2021-07-28 17:37:15 -0400

    Add files via upload

---
+++ About/js/values.js
@@ -0,0 +1,40 @@
+// ---------------------------------------- //
+// VARIABLES
+// ---------------------------------------- //
+
+// Buttons
+var partnershipsButton = document.getElementById("partnerships");
+var creativityButton = document.getElementById("creativity");
+var generosityButton = document.getElementById("generosity");
+var excellenceButton = document.getElementById("excellence");
+var friendlinessButton = document.getElementById("friendliness");
+
+// Display
+var valueDisplay = document.getElementById("display");
+
+// Images
+var partnershipsImage = document.createElement("img");
+partnershipsImage.src = "./Img/test.jpg";
+
+// ---------------------------------------- //
+// FUNCTIONS
+// ---------------------------------------- //
+partnershipsButton.onclick = function() {
+    // Place code here
+};
+
+creativityButton.onclick = function() {
+    // Place code here
+};
+
+generosityButton.onclick = function() {
+    // Place code here
+};
+
+excellenceButton.onclick = function() {
+    // Place code here
+};
+
+friendlinessButton.onclick = function() {
+    // Place code here
+};

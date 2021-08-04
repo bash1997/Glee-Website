33c232f90d15d1b9a69fd4e40f814aa2423d15f4
Bradley Lau <79525926+BradLau206@users.noreply.github.com>
2021-08-04 18:01:32 -0400

    Add files via upload

---
+++ About/js/beliefs.js
@@ -0,0 +1,44 @@
+// ---------------------------------------- //
+// VARIABLES
+// ---------------------------------------- //
+
+// Buttons
+var trinityButton = document.getElementById("trinity");
+var bibleButton = document.getElementById("bible");
+var jesusButton = document.getElementById("jesus");
+var salvationButton = document.getElementById("salvation");
+var sacramentsButton = document.getElementById("sacraments");
+var holySpiritButton = document.getElementById("holy-spirit");
+var churchButton = document.getElementById("church");
+
+// ---------------------------------------- //
+// FUNCTIONS
+// ---------------------------------------- //
+
+function trinityClick() {
+    console.log("trinity button works");
+}
+
+function bibleClick() {
+    console.log("bible button works");
+}
+
+function jesusClick() {
+    console.log("jesus button works");
+}
+
+function salvationClick() {
+    console.log("salvation button works");
+}
+
+function sacramentsClick() {
+    console.log("sacraments button works");
+}
+
+function holySpiritClick() {
+    console.log("holy spirit button works");
+}
+
+function churchClick() {
+    console.log("church button works");
+}

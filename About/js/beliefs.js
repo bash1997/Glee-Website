811c96437abc00e231ae711b3685f170a8a9701c
Bradley Lau <79525926+BradLau206@users.noreply.github.com>
2021-09-22 18:37:53 -0400

    Update beliefs.js

--- About/js/beliefs.js
+++ About/js/beliefs.js
@@ -11,34 +11,76 @@ var sacramentsButton = document.getElementById("sacraments");
 var holySpiritButton = document.getElementById("holy-spirit");
 var churchButton = document.getElementById("church");
 
+// Display Properties
+var selectedImg = document.getElementById("display");
+var selectedValue = document.getElementById("selected-belief");
+var valueInfo = document.getElementById("belief-info");
+
+// Image Sources
+const ImgTrinity = "";
+const ImgBible = "";
+const ImgJesus = "";
+const ImgSalvation = "";
+const ImgSacraments = "";
+const ImgHolySpirit = "";
+const ImgChurch = "";
+
+// Belief Titles
+const TitleTrinity = "The Trinity";
+const TitleBible = "The Bible";
+const TitleJesus = "Jesus";
+const TitleSalvation = "Salvation";
+const TitleSacraments = "Sacraments";
+const TitleHolySpirit = "The Holy Spirit";
+const TitleChurch = "The Church";
+
+// Belief Info
+const InfoTrinity = "1";
+const InfoBible = "2";
+const InfoJesus = "3";
+const InfoSalvation = "4";
+const InfoSacraments = "5";
+const InfoHolySpirit = "6";
+const InfoChurch = "7";
+
 // ---------------------------------------- //
 // FUNCTIONS
 // ---------------------------------------- //
 
 function trinityClick() {
-    console.log("trinity button works");
+    swap(ImgTrinity,TitleTrinity,InfoTrinity);
 }
 
 function bibleClick() {
-    console.log("bible button works");
+    swap(ImgBible,TitleBible,InfoBible);
 }
 
 function jesusClick() {
-    console.log("jesus button works");
+    swap(ImgJesus,TitleJesus,InfoJesus);
 }
 
 function salvationClick() {
-    console.log("salvation button works");
+    swap(ImgSalvation,TitleSalvation,InfoSalvation);
 }
 
 function sacramentsClick() {
-    console.log("sacraments button works");
+    swap(ImgSacraments,TitleSacraments,InfoSacraments);
 }
 
 function holySpiritClick() {
-    console.log("holy spirit button works");
+    swap(ImgHolySpirit,TitleHolySpirit,InfoHolySpirit);
 }
 
 function churchClick() {
-    console.log("church button works");
+    swap(ImgChurch,TitleChurch,InfoChurch);
 }
+
+// ---------------------------------------- //
+// HELPER FUNCTIONS
+// ---------------------------------------- //
+
+function swap(imgSrc, value, info) {
+    selectedImg.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)) , url('${imgSrc}')`;
+    selectedValue.innerText = value;
+    valueInfo.innerText = info;
+ }

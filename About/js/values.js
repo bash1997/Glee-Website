355a675561caae0db0c63c6464613aa7eef6de3e
Bradley Lau <79525926+BradLau206@users.noreply.github.com>
2021-09-29 18:42:29 -0400

    Update values.js

--- About/js/values.js
+++ About/js/values.js
@@ -29,11 +29,11 @@ const TitleExcellence = "Excellence";
 const TitleFriendliness = "Friendliness";
 
 // Value Info
-const InfoPartnerships = "Glee Church values its many partnerships with the community.";
-const InfoCreativity = "We at Glee Church strive to be imaginative in all we do.";
-const InfoGenerosity = "Giving back to our community is what Glee Church strives to achieve.";
-const InfoExcellence = "We at Glee Church seek to be the best version of ourselves.";
-const InfoFriendliness = "Friendship is what makes the Glee Church Community.";
+const InfoPartnerships = "We Love Community.\n We don’t allow our ego or personal agenda to get in the way of doing what’s best for our community. We are committed to each other and always willing to form new partnerships or help volunteer when that’s what’s needed for success. Partnerships are always worth the effort. We win and lose as a community.";
+const InfoCreativity = "We Aim to be Relevant.\n We demonstrate a relentless solution focus. Together we share our experiences and differences to learn and think more efficiently. We aim to remain relevant by sharing an unconventional approach to minister the Gospel of Jesus Christ!";
+const InfoGenerosity = "We inspire to Give More.\n We make no excuses for why something can't be done! We strongly believe where there is a will there is away. We regularly seek opportunities to assist those in need, to give to our church and community. Demonstrating a tangible approach to giving back, we put the needs of others ahead of our own.";
+const InfoExcellence = "We Purse Excellence.\n Everything speaks. Details matter. We hold ourselves to the high standard to produce excellence in everything we do. First impressions only happen once. Every interaction that people have with Glee Church has the opportunity to influence how they relate to God. The goal is to get things “right”, not simply to get things “done”. For us, that means sweating the small stuff in pursuit of excellence";
+const InfoFriendliness = "We Choose to be Happy.\n Smile. Laugh every day. While our passion for excellence is real, sometimes that’s easy and sometimes it’s hard. We consider the work we do a “privilege” and not an “obligation”. First impressions only happen once. No matter the role we play, we choose to show up with the right spirit and right attitude to be an enjoyable breath of life.";
 
 // ---------------------------------------- //
 // BUTTON FUNCTIONS

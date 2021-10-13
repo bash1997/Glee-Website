307cb0409a80cb317dee74e5458adf53a30fb3d5
Bradley Lau <79525926+BradLau206@users.noreply.github.com>
2021-10-13 18:53:29 -0400

    Add files via upload

---
+++ About/js/leadership.js
@@ -0,0 +1,10 @@
+const btnPrev = document.getElementById("previous");
+const btnCurr = document.getElementById("current");
+
+function clickPrev() {
+    console.log("Previous Button Working");
+}
+
+function clickCurr() {
+    console.log("Current Button Working");
+}

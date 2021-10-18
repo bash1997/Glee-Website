8b83ac0b2e5c8a30649a022c9cd9f9670b421ca7
Bradley Lau <79525926+BradLau206@users.noreply.github.com>
2021-10-18 18:29:04 -0400

    Update leadership.js

--- About/js/leadership.js
+++ About/js/leadership.js
@@ -1,10 +1,43 @@
+class Leader {
+    constructor(title,name,description) {
+        this.title = title;
+        this.name = name;
+        this.description = description;
+    }
+
+    get title() {
+        return this.title;
+    }
+
+    get name() {
+        return this.name;
+    }
+
+    get description() {
+        return this.description;
+    }
+}
+
 const btnPrev = document.getElementById("previous");
 const btnCurr = document.getElementById("current");
 
+var testArray = ['e1','e2','e3','e4','e5','e6','e7'];
+var index = 0;
+const max = testArray.length-1;
+
 function clickPrev() {
-    console.log("Previous Button Working");
+    index--;
+    indexCheck();
+    console.log(testArray[index]);
 }
 
 function clickCurr() {
-    console.log("Current Button Working");
+    index++;
+    indexCheck();
+    console.log(testArray[index]);
+}
+
+function indexCheck() {
+    if(index > max) {index = 0;}
+    else if(index <= 0) {index = max;}
 }
